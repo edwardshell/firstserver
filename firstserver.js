67cbfae5d9c4884fa7611f3059d03983e839e264
edwardshell <37218249+edwardshell@users.noreply.github.com>
2018-07-06 11:17:01 -0400

    Update firstserver.js

--- firstserver.js
+++ firstserver.js
@@ -11,5 +11,5 @@ var server = http.createServer(handleRequest);
 
 server.listen(PORT, function() {
 
-    console.log("Server listening on: http://localhost" + PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
 })
